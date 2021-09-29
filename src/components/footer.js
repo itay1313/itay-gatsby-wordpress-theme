89fe628d053c28def348736f32339f3997bca27f
itay haephrati <itay1313@gmail.com>
2021-09-29 12:27:45 +0300

    footertext

--- src/components/footer.js
+++ src/components/footer.js
@@ -47,12 +47,14 @@ const Footer = () => {
             </Link>
             <div className="mt-4 lg:max-w-sm">
               <p className="text-sm text-deep-purple-50">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam.
+                I am developing or Designing websites for small businesses and
+                entrepreneurs. Whether it’s on the frontend or in the user
+                experience,
               </p>
               <p className="mt-4 text-sm text-deep-purple-50">
-                Eaque ipsa quae ab illo inventore veritatis et quasi architecto
-                beatae vitae dicta sunt explicabo.
+                the centerpiece of my career has been materializing my client’s
+                vision on the “big screen.” Essentially, what they see in color,
+                I bring to life in code.
               </p>
             </div>
           </div>
