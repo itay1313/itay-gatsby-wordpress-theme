4a54de463aa9d688bea148575f829477bcd0b904
itay haephrati <itay1313@gmail.com>
2021-09-29 12:36:30 +0300

    footertextcolor

--- src/components/footer.js
+++ src/components/footer.js
@@ -41,7 +41,7 @@ const Footer = () => {
                 <rect x="14" y="1" width="7" height="6" />
                 <rect x="14" y="11" width="7" height="12" />
               </svg>
-              <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+              <span className="ml-2 text-xl font-bold tracking-wide uppercase">
                 ItayCode
               </span>
             </Link>
